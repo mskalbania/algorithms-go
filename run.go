46a739332f4f50899f33a267cb67bdfa8da05141
mskalbania <mateusz.skalbania1@tesco.com>
2024-07-27 21:12:01 +0200

    Add Day3

--- run.go
+++ run.go
@@ -1,10 +1,9 @@
 package main
 
-import (
-	"hackerrank-go/solutions"
-)
+import "hackerrank-go/solutions"
 
 func main() {
 	//solutions.Day1()
-	solutions.Day2()
+	//solutions.Day2()
+	solutions.Day3()
 }
---
+++ solutions/day3.go
@@ -0,0 +1,31 @@
+package solutions
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Day3() {
+	result := timeConversion("13:00:01PM")
+	fmt.Printf("%s", result)
+}
+
+/*
+Converts 12h to 24h format
+*/
+func timeConversion(s string) string {
+	suffix := s[len(s)-2:]
+	time := s[0 : len(s)-2]
+	hour, _ := strconv.Atoi(time[0:2])
+	if suffix == "AM" {
+		if hour == 12 {
+			return "00" + time[2:]
+		}
+		return time
+	} else {
+		if hour == 12 {
+			return time
+		}
+		return strconv.Itoa(hour+12) + time[2:]
+	}
+}
