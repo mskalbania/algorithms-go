15fe10fd6588916f59411b0bdede16f834b32a52
mskalbania <mateusz.skalbania1@tesco.com>
2024-07-28 09:23:36 +0200

    Add Day4

--- run.go
+++ run.go
@@ -5,5 +5,6 @@ import "hackerrank-go/solutions"
 func main() {
 	//solutions.Day1()
 	//solutions.Day2()
-	solutions.Day3()
+	//solutions.Day3()
+	solutions.Day4()
 }
---
+++ solutions/Day4.go
@@ -0,0 +1,26 @@
+package solutions
+
+import "fmt"
+
+func Day4() {
+	fmt.Printf("%v", breakingRecords([]int32{12, 24, 10, 24}))
+}
+
+/*
+Solution to:
+https://www.hackerrank.com/challenges/three-month-preparation-kit-breaking-best-and-worst-records/problem
+*/
+func breakingRecords(scores []int32) []int32 {
+	minScore, maxScore, timesMin, timesMax := scores[0], scores[0], int32(0), int32(0)
+	for i := 1; i < len(scores); i++ {
+		if scores[i] > maxScore {
+			maxScore = scores[i]
+			timesMax++
+		}
+		if scores[i] < minScore {
+			minScore = scores[i]
+			timesMin++
+		}
+	}
+	return []int32{timesMax, timesMin}
+}
